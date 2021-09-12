267627e2395cab2e2eab88df5d44ed6c25dbd4da
DJ Nrrd <djnrrd@gmail.com>
2021-09-12 17:43:10 +0100

    Stripped bot down to the bare minimum

--- obs_sd_controls/twitch_controls.py
+++ obs_sd_controls/twitch_controls.py
@@ -6,24 +6,24 @@ class TwitchBatchBot(SingleServerIRCBot):
     VERSION = '0.2.0'
 
     def __init__(self, nickname, token):
+        """A simple bot that logs into the twitch user's own channel to run a
+        batch of commands before logging out again.
+
+        :param nickname: The user's twitch logon
+        :type nickname: str
+        :param token: The user's OAUTH token
+        :type token: str
+        :cvar channel: The user's chat channel
+        """
         token = f"oauth:{token}"
         super().__init__([('irc.twitch.tv', 6667, token)], nickname,
                          nickname)
         self.channel = nickname
-        self.viewers = []
 
     def on_welcome(self, connection, event):
         connection.join(self.channel)
         connection.privmsg(event.target, '/followers 1d')
         self.die('Locked down chat')
-
-    def on_disconnect(self, connection, event):
-        pass
-
-    @staticmethod
-    def _parse_nickname_from_twitch_user_id(user_id):
-        # nickname!username@nickname.tmi.twitch.tv
-        return user_id.split('!', 1)[0]
 
 
 def start_stop_safety(username, token):
