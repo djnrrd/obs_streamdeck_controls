01e52876c1b56bde38b7089b2de8a189e8136700
DJ Nrrd <djnrrd@gmail.com>
2021-09-16 22:27:25 +0100

    Reformat package to conform to the Python guidelines for packaging projects.  As part of this, keep the version number in conf.py so that it can be read in by setup.py and Sphinx autodocumentation as well

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src/'))
+from obs_sd_controls.conf import VERSION
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +23,7 @@ copyright = '2021, DJ Nrrd'
 author = 'DJ Nrrd'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = VERSION
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
 from setuptools import setup
+import os
+import sys
+sys.path.extend([os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'src')])
+from obs_sd_controls.conf import VERSION
 
 setup(
-    name='obs_streamdeck_controls',
-    version='0.2.3',
-    install_requires=['simpleobsws', 'appdirs', 'irc'],
-    packages=['obs_sd_controls'],
-    entry_points={
-        'console_scripts': ['obs-streamdeck-ctl=obs_sd_controls.cli_entry:main']
-    },
-    include_package_data=True,
-    url='https://github.com/djnrrd/obs_streamdeck_controls',
-    license='GPL-3.0 License',
-    author='djnrrd',
-    author_email='djnrrd@gmail.com',
-    description='A command line tool for controlling OBS Studio via the '
-                'OBS WebSockets API',
-    keywords='twitch streamdeck obs',
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Topic :: Games/Entertainment',
-        'Topic :: Home Automation',
-    ]
+    version=VERSION,
 )
