bc39c9b8fa1ec5af8f9a10815a814ec1d13114c9
DJ Nrrd <djnrrd@gmail.com>
2021-09-16 18:33:37 +0100

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='obs_streamdeck_controls',
-    version='0.2.2',
+    version='0.2.3',
     install_requires=['simpleobsws', 'appdirs', 'irc'],
     packages=['obs_sd_controls'],
     entry_points={
